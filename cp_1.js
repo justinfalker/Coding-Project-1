cc6b5abcccff4c6204a950031f61d2bfe4b12fd0
Justin Falker <falkerj@usf.edu>
2026-03-12 11:49:18 -0400

    Index.html file change + const creations

---
+++ cp_1.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("form");
+const count = document.getElementById("count");
+const tooltip = document.getElementById("tooltip");
+const feedback = document.getElementById("feedback-display");
