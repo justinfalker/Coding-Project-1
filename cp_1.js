c8279284bbb6b3013c3ecd9c8d93a626a4499355
Justin Falker <falkerj@usf.edu>
2026-03-12 11:51:04 -0400

    Step 5

--- cp_1.js
+++ cp_1.js
@@ -2,3 +2,17 @@ const form = document.getElementById("form");
 const count = document.getElementById("count");
 const tooltip = document.getElementById("tooltip");
 const feedback = document.getElementById("feedback-display");
+
+document.body.addEventListener("click", function () {
+  console.log("background");
+});
+
+form.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
+form.addEventListener("input", function (e) {
+  if (e.target.id === "comments") {
+    count.textContent = e.target.value.length;
+  }
+});
