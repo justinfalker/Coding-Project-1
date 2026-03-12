1b77d20d861822496ac4a86a95e5b09c0cbe743e
Justin Falker <falkerj@usf.edu>
2026-03-12 12:01:57 -0400

    DOM Event Listeners

--- cp_1.js
+++ cp_1.js
@@ -28,3 +28,40 @@ form.addEventListener("mouseout", function (e) {
     tooltip.textContent = "";
   }
 });
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let comments = document.getElementById("comments").value.trim();
+
+  document.getElementById("nameError").textContent = "";
+  document.getElementById("emailError").textContent = "";
+  document.getElementById("commentsError").textContent = "";
+
+  let valid = true;
+
+  if (!name) {
+    document.getElementById("nameError").textContent = "Required";
+    valid = false;
+  }
+  if (!email) {
+    document.getElementById("emailError").textContent = "Required";
+    valid = false;
+  }
+  if (!comments) {
+    document.getElementById("commentsError").textContent = "Required";
+    valid = false;
+  }
+
+  if (valid) {
+    const entry = document.createElement("p");
+    entry.textContent = name + " " + email + " " + comments;
+
+    feedback.appendChild(entry);
+
+    form.reset();
+    count.textContent = "0";
+  }
+});
